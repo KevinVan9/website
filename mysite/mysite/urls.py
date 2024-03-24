af63c748e502a68d6c85ea6feaf92ca669136224
Kevin <infinitex456@gmail.com>
2024-03-24 11:40:34 +1100

    cleanup navbar uri; add favicon

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('polls/')),
+    path('favicon.ico', lambda request: redirect('static/polls/favicon.ico')),
 ]
